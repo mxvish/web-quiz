8cde0390a13780a05d67ec99348d0c24179a3648
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2022-10-05 19:35:44 +0900

    Add files via upload

--- answer.php
+++ answer.php
@@ -11,86 +11,63 @@
     </head>
     <body>
         <?php  
-//        class Question {
-//            public $quiz;
-//            public $answer;
-//            public $score;
-//            public function __construct($quiz, $answer) {
-//                $this->quiz = $quiz;
-//                $this->answer = $answer;
-//                $this->score = check($_POST[$quiz], $answer);
-//            }
-//        
-//            public function message() {
-//                return "My car is a " . $this->quiz . " " . $this->answer . "!";
-//            }
-//        }
-// refer to https://www.w3schools.com/php/php_oop_classes_objects.asp ?
-        
-        //$q1 = new Question("q1", 1);
-        //echo $myQuestion -> message();
-        //$myQuestion = new Question("red", "Toyota");
-        //echo $myQuestion -> message();
+        class Question {
+            public $quiz;
+            public $answer;
+            public $score;
 
-        function check($input, $ans){
-            if($input == $ans){
-                return 10;
-            } else {
-                return 0;
+            public function __construct($quiz, $answer) {
+                $this->quiz = $quiz;
+                $this->answer = $answer;
+            }
+        
+            function set_score() {
+                if($_POST[$this->quiz] == $this->answer){
+                    $this->score = 10;
+                } else {
+                    $this->score = 0;
+                }
+            }
+
+            function get_score() {
+                return $this->score;
+            }
+
+            function print_explanation($text) {
+                echo '<h2>' . strtoupper($this->quiz) . '</h2>';
+                if($this->score == 10){
+                    echo '<h3 style="color:DodgerBlue">正解！</h3>';
+                } else {
+                    echo '<h3 style="color:red">不正解</h3>';
+                }
+        
+                echo '
+                <h3>あなたの回答：' . $_POST[$this->quiz] . '</h3>
+                <h3>正解：' . $this->answer . '</h3>
+        
+                <center><div style="white-space:pre"><b>解説</b><br>' . $text . '
+                </div></center><br style="line-height:100px;">';
             }
         }
-
-        $quiz="q1";
-        $q1answer=1;
-        $q1score=check($_POST[$quiz], $q1answer);
-
-        echo '<h1>正解発表</h1>
-        <h2>' . strtoupper($quiz) . '</h2>';
-        if($q1score == 10){
-            echo '<h3 style="color:DodgerBlue">正解！</h3>';
-        } else {
-            echo '<h3 style="color:red">不正解</h3>';
-        }
-
-        echo '
-        <h3>あなたの回答：' . $_POST[$quiz] . '</h3>
-        <h3>正解：' . $q1answer . '</h3>
-
-        <center><div style="white-space:pre">
-        <b>解説</b><br>
-        浜松キャンパスの住所は浜松市中区城北3-5-1です。
-        詳しくは下のページを参考にしてください。
-        <a href="https://www.shizuoka.ac.jp/access/#h2%20under">交通アクセス｜静岡大学</a>
-        </div></center><br style="line-height:100px;">';
-
-
-        $quiz="q2";
-        $q2answer="wait";
-        $q2score=check($_POST["q2"], $q2answer);
-
-        echo '<h2>'. strtoupper($quiz) . '</h2>';
-        if($q2score == 10){
-            echo '<h3 style="color:DodgerBlue">正解！</h3>';
-        } else {
-            echo '<h3 style="color:red">不正解</h3>';
-        }
-
-        echo '
-        <h3>あなたの回答：' . $_POST[$quiz] . '</h3>
-        <h3>正解：' . $q2answer . '</h3>
-
-        <center><div style="white-space:pre">
-        <b>解説</b><br>
-        w8はwaitのことです。
-        スラングではwaitのaitを8と表記します。
-        </div></center>';
         
+        echo '<h1>正解発表</h1>';
+
+        $q1 = new Question("q1", 1);
+        $q1->set_score();
+        $q1->print_explanation('浜松キャンパスの住所は浜松市中区城北3-5-1です。
+詳しくは下のページを参考にしてください。
+<a href="https://www.shizuoka.ac.jp/access/#h2%20under">交通アクセス｜静岡大学</a>');
+
+        $q2 = new Question("q2", "wait");
+        $q2->set_score();
+        $q2->print_explanation('w8はwaitのことです。
+スラングではwaitのaitを8と表記します。');
+
         $conn = mysqli_connect("localhost", "", "", "quiz");
         mysqli_set_charset($conn, "utf8");
         $sql = 'INSERT INTO quiz(q1,q2) VALUES (?, ?)';  
         $stmt = $conn->prepare($sql);
-        echo $scores[0];
-        $stmt->bind_param("is", $q1score, $q2score);
+        $stmt->bind_param("is", $q1->get_score(), $q2->get_score());
         $stmt->execute();
 
         $stmt->close();
